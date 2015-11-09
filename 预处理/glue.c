6358b8c32baab8fe38365f11409a681572a8efe9
YinXinLION <yinxin19950816@gmail.com>
2015-11-09 21:50:38 +0800

    使用##运算符

--- 预处理/glue.c
+++ 预处理/glue.c
@@ -16,7 +16,7 @@ int main(void)
 {
     int XNAME(1) = 14; //变为int x1 = 14;
     int XNAME(2) = 20;//变为int x2 = 20;
-    PRINT_XN(1); //变为printf("x1 = %d\n",x1)
+    PRINT_XN(1); //变为printf("x1 = %d\n",x1);
     PRINT_XN(2); //变为printf("x2 = %d\n",x2);
 
     return 0;
